bcb47058080c6a5bdf70ede84fd7e61069c67b8d
zgrummons <zgrummons@gmail.com>
2021-10-02 14:29:35 -0500

    Revert "mvp"

--- Responsivity/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Config
-{
-    public string DefaultMailAddress { get; set; }
-    public string DefaultMailName { get; set; }
-    public string FromMailAddress {  get; set; }
-    public string FromMailName { get; set; }
-    public string Environment { get; set; }
-}
